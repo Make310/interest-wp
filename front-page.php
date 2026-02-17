b9302ac3d4182e60dab22cd039290c0ba565dec6
Make310 <luisfmarquez1999@gmail.com>
2026-02-17 15:04:45 -0500

    home mobile style

--- front-page.php
+++ front-page.php
@@ -7,13 +7,20 @@
             <div class="hero">
                 <div class="hero__content">
                     <h1 class="hero__title">
-                        Lower interest rate <span class="underline">guarantee</span><br>
-                        on your home mortgage
+                        <span class="hero__title-line hero__title-line--desktop">Lower interest rate <span class="underline">guarantee</span></span><br class="hero__break hero__break--desktop">
+                        <span class="hero__title-line hero__title-line--desktop">on your home mortgage</span>
+                        <span class="hero__title-line hero__title-line--mobile">Lowest interest rate</span>
+                        <span class="hero__title-line hero__title-line--mobile"><span class="underline">gauranteed</span>, on your</span>
+                        <span class="hero__title-line hero__title-line--mobile">home mortgage</span>
                     </h1>
                     <p class="hero__text">
-                        Fast and Free consultation, use<br>
-                        Interest Rate Research for the lowest<br>
-                        interest rate available today from the top lenders
+                        <span class="hero__text-line hero__text-line--desktop">Fast and Free consultation, use</span><br class="hero__break hero__break--desktop">
+                        <span class="hero__text-line hero__text-line--desktop">Interest Rate Research for the lowest</span><br class="hero__break hero__break--desktop">
+                        <span class="hero__text-line hero__text-line--desktop">interest rate available today from the top lenders.</span>
+                        <span class="hero__text-line hero__text-line--mobile">Fast and Free consultation, use</span>
+                        <span class="hero__text-line hero__text-line--mobile">Interest Rate Research for the</span>
+                        <span class="hero__text-line hero__text-line--mobile">lowest interest rate available</span>
+                        <span class="hero__text-line hero__text-line--mobile">today from the top lenders.</span>
                     </p>
                 </div>
                 <div class="hero__image">
@@ -40,11 +47,19 @@
         <div class="card card--guarantee">
             <div class="guarantee">
                 <div class="guarantee__content">
-                    <h1 class="guarantee__title">Lower Interest Rate Guarantee</h1>
+                    <h1 class="guarantee__title">
+                        <span class="guarantee__title-line guarantee__title-line--desktop">Lower Interest Rate Guarantee</span>
+                        <span class="guarantee__title-line guarantee__title-line--mobile">Lower</span>
+                        <span class="guarantee__title-line guarantee__title-line--mobile">Interest Rate</span>
+                        <span class="guarantee__title-line guarantee__title-line--mobile">Gaurantee</span>
+                    </h1>
                     <p class="guarantee__text">
-                        Our Lower Interest Rate Guarantee is unique and built on research.
-                        Through a nationwide network of mortgage professionals, we focus on
-                        providing access to the lowest mortgage interest rate in the market.
+                        <span class="guarantee__text-line guarantee__text-line--desktop">Our Lower Interest Rate Guarantee is unique and built on research. Through a nationwide network of mortgage professionals, we focus on providing access to the lowest mortgage interest rate in the market.</span>
+                        <span class="guarantee__text-line guarantee__text-line--mobile">Our Lower Interest Rate Guarantee</span>
+                        <span class="guarantee__text-line guarantee__text-line--mobile">is unique and built on research. We</span>
+                        <span class="guarantee__text-line guarantee__text-line--mobile">focus on providing access to the</span>
+                        <span class="guarantee__text-line guarantee__text-line--mobile">lowest mortgage interest rate in the</span>
+                        <span class="guarantee__text-line guarantee__text-line--mobile">market.</span>
                     </p>
                     <a href="<?php echo home_url('/get-started'); ?>" class="btn btn--bordered">
                         GET STARTED <span>&rarr;</span>
@@ -75,11 +90,20 @@
                     </div>
                 </div>
 
-                <h2 class="step__subtitle">Have you recieved a Loan Estimate?</h2>
+                <h2 class="step__subtitle">
+                    <span class="step-one__subtitle-line step-one__subtitle-line--desktop">Have you recieved a Loan Estimate?</span>
+                    <span class="step-one__subtitle-line step-one__subtitle-line--mobile">Have you recieved a</span>
+                    <span class="step-one__subtitle-line step-one__subtitle-line--mobile">Loan Estimate?</span>
+                </h2>
                 <p class="step__text">
-                    To use or lower rate gaurantee click the icon above to upload your first offer<br>
-                    <em>Safe and secure, your information will never be sold or misused here.<br>
-                    You will receive a certified counter offer by the end of the business day</em>
+                    <span class="step-one__text-line step-one__text-line--desktop">To use or lower rate gaurantee click the icon above to upload your first offer</span><br class="step-one__break step-one__break--desktop">
+                    <span class="step-one__text-line step-one__text-line--desktop"><em>Safe and secure, your information will never be sold or misused here.</em></span><br class="step-one__break step-one__break--desktop">
+                    <span class="step-one__text-line step-one__text-line--desktop"><em>You will receive a certified counter offer by the end of the business day</em></span>
+                    <span class="step-one__text-line step-one__text-line--mobile">To use or lowest rate gaurantee click</span>
+                    <span class="step-one__text-line step-one__text-line--mobile">the icon above to upload your first</span>
+                    <span class="step-one__text-line step-one__text-line--mobile">offer. Safe and secure, you will receive</span>
+                    <span class="step-one__text-line step-one__text-line--mobile">a certified counter offer by the end of</span>
+                    <span class="step-one__text-line step-one__text-line--mobile">the business day.</span>
                 </p>
                 <a href="#" class="step__faq-link">FAQ's on uploading your loan estimate</a>
             </div>
@@ -93,8 +117,11 @@
                 <span class="step__label">STEP TWO</span>
                 <h1 class="step__title">Compare rates</h1>
                 <p class="step__intro">
-                    Fast and Free, use Interest Rate Research<br>
-                    for the lowest inPut our gaurnanteterest
+                    <span class="step-two__intro-line step-two__intro-line--desktop">Fast and Free, use Interest Rate Research</span><br class="step-two__break step-two__break--desktop">
+                    <span class="step-two__intro-line step-two__intro-line--desktop">for the lowest inPut our gaurnanteterest</span>
+                    <span class="step-two__intro-line step-two__intro-line--mobile">Fast and Free, use Interest</span>
+                    <span class="step-two__intro-line step-two__intro-line--mobile">Rate Research for the lowest</span>
+                    <span class="step-two__intro-line step-two__intro-line--mobile">inPut our gaurnanteterest</span>
                 </p>
 
                 <div class="step__features">
@@ -102,8 +129,11 @@
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/shaking hands icon.png" alt="Lender Network" class="feature__icon">
                         <h3 class="feature__title">Over 30 participating lenders</h3>
                         <p class="feature__text">
-                            You will benefit from real time pricing<br>
-                            from over 30 lenders nationwide
+                            <span class="step-two__feature-line step-two__feature-line--desktop">You will benefit from real time pricing</span><br class="step-two__break step-two__break--desktop">
+                            <span class="step-two__feature-line step-two__feature-line--desktop">from over 30 lenders nationwide</span>
+                            <span class="step-two__feature-line step-two__feature-line--mobile">You will benefit from real time</span>
+                            <span class="step-two__feature-line step-two__feature-line--mobile">pricing from over 30 lenders</span>
+                            <span class="step-two__feature-line step-two__feature-line--mobile">nationwide</span>
                         </p>
                     </div>
 
@@ -111,8 +141,11 @@
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/equality icon.png" alt="Lowest Rate" class="feature__icon">
                         <h3 class="feature__title">Gauranteed lowest rate</h3>
                         <p class="feature__text">
-                            Fast and Free, use Interest Rate Research<br>
-                            for the lowest inPut our gaurnanteterest
+                            <span class="step-two__feature-line step-two__feature-line--desktop">Fast and Free, use Interest Rate Research</span><br class="step-two__break step-two__break--desktop">
+                            <span class="step-two__feature-line step-two__feature-line--desktop">for the lowest inPut our gaurnanteterest</span>
+                            <span class="step-two__feature-line step-two__feature-line--mobile">Fast and Free, use Interest</span>
+                            <span class="step-two__feature-line step-two__feature-line--mobile">Rate Research for the lowest</span>
+                            <span class="step-two__feature-line step-two__feature-line--mobile">inPut our gaurnanteterest</span>
                         </p>
                     </div>
                 </div>
@@ -132,10 +165,14 @@
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Rate Guarantee Seal.png" alt="Rate Guarantee Seal" class="step__seal-img">
                 </div>
 
-                <h2 class="step__subtitle">You got the lower rate!</h2>
+                <h2 class="step__subtitle">You got the lowest rate!</h2>
                 <p class="step__text">
-                    Your IRR Pro will work with you through closing<br>
-                    Fast and efficient. Read what our customers have to say &rarr;
+                    <span class="step-three__text-line step-three__text-line--desktop">Your IRR Pro will work with you through closing</span><br class="step-three__break step-three__break--desktop">
+                    <span class="step-three__text-line step-three__text-line--desktop">Fast and efficient. Read what our customers have to say &rarr;</span>
+                    <span class="step-three__text-line step-three__text-line--mobile">Your IRR Pro will work with you</span>
+                    <span class="step-three__text-line step-three__text-line--mobile">through closing Fast and</span>
+                    <span class="step-three__text-line step-three__text-line--mobile">efficient. Read what our</span>
+                    <span class="step-three__text-line step-three__text-line--mobile">customers have to say &rarr;</span>
                 </p>
 
                 <p class="step__tagline">
@@ -153,33 +190,64 @@
     <section class="section">
         <div class="card">
             <div class="research">
+                <span class="research__label research__label--mobile">RESEARCH EXCERPTS</span>
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/house low 2.png" alt="House" class="research__house">
-                <h1 class="research__title">Benefits of shopping around<br>when buying a home</h1>
-                <p class="research__subtitle">A center-aligned collection of statements from neutral, authoritative institutions.</p>
+                <h1 class="research__title">
+                    <span class="research__title-line research__title-line--desktop">Benefits of shopping around</span><br class="research__break research__break--desktop">
+                    <span class="research__title-line research__title-line--desktop">when buying a home</span>
+                    <span class="research__title-line research__title-line--mobile">Benefits of shopping</span>
+                    <span class="research__title-line research__title-line--mobile">around when buying</span>
+                </h1>
+                <p class="research__subtitle">
+                    <span class="research__subtitle-line research__subtitle-line--desktop">A center-aligned collection of statements from neutral, authoritative institutions.</span>
+                    <span class="research__subtitle-line research__subtitle-line--mobile">A collection of statements from neutral,</span>
+                    <span class="research__subtitle-line research__subtitle-line--mobile">authoritative institutions.</span>
+                </p>
 
                 <div class="research__divider"></div>
 
                 <div class="research__quotes">
                     <div class="quote">
-                        <p class="quote__text">"Shopping for a mortgage could save you thousands of dollars."</p>
+                        <p class="quote__text">
+                            <span class="research__quote-line research__quote-line--desktop">"Shopping for a mortgage could save you thousands of dollars."</span>
+                            <span class="research__quote-line research__quote-line--mobile">“Shopping for a mortgage could save you</span>
+                            <span class="research__quote-line research__quote-line--mobile">thousands of dollars.”</span>
+                        </p>
                         <span class="quote__source">Consumer Financial Protection Bureau</span>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cfpb.png" alt="CFPB" class="quote__logo">
                     </div>
 
                     <div class="quote">
-                        <p class="quote__text">"Consumers who shop around for rates and fees may save<br>thousands of dollars over the life of a loan."</p>
+                        <p class="quote__text">
+                            <span class="research__quote-line research__quote-line--desktop">"Consumers who shop around for rates and fees may save</span><br class="research__break research__break--desktop">
+                            <span class="research__quote-line research__quote-line--desktop">thousands of dollars over the life of a loan."</span>
+                            <span class="research__quote-line research__quote-line--mobile">“Consumers who shop around for rates and fees</span>
+                            <span class="research__quote-line research__quote-line--mobile">may save thousands of dollars over the life of a</span>
+                            <span class="research__quote-line research__quote-line--mobile">loan.”</span>
+                        </p>
                         <span class="quote__source">Federal Trade Commission</span>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ftc.png" alt="FTC" class="quote__logo">
                     </div>
 
                     <div class="quote">
-                        <p class="quote__text">"You are not required to use the lender or settlement service<br>providers recommended by your real estate agent."</p>
+                        <p class="quote__text">
+                            <span class="research__quote-line research__quote-line--desktop">"You are not required to use the lender or settlement service</span><br class="research__break research__break--desktop">
+                            <span class="research__quote-line research__quote-line--desktop">providers recommended by your real estate agent."</span>
+                            <span class="research__quote-line research__quote-line--mobile">“You are not required to use the lender or</span>
+                            <span class="research__quote-line research__quote-line--mobile">settlement service providers recommended by</span>
+                            <span class="research__quote-line research__quote-line--mobile">your real estate agent.”</span>
+                        </p>
                         <span class="quote__source">U.S. Department of Housing and Urban Development</span>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hud.png" alt="HUD" class="quote__logo">
                     </div>
 
                     <div class="quote">
-                        <p class="quote__text">"Comparing loan offers from multiple lenders can help borrowers find the mortgage that best meets their needs."</p>
+                        <p class="quote__text">
+                            <span class="research__quote-line research__quote-line--desktop">"Comparing loan offers from multiple lenders can help borrowers find the mortgage that best meets their needs."</span>
+                            <span class="research__quote-line research__quote-line--mobile">“Comparing loan offers from multiple lenders can</span>
+                            <span class="research__quote-line research__quote-line--mobile">help borrowers find the mortgage that best meets</span>
+                            <span class="research__quote-line research__quote-line--mobile">their needs.”</span>
+                        </p>
                         <span class="quote__source">Federal Reserve</span>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/fed.png" alt="Federal Reserve" class="quote__logo">
                     </div>
